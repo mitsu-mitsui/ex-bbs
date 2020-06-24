47ba2db62c58eeac02c9b52fb8e0584aa9ca78c1
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-24 14:32:08 +0900

    全投稿内容を表示html実装

--- src/main/java/com/example/Controller/ArticleController.java
+++ src/main/java/com/example/Controller/ArticleController.java
@@ -1,9 +1,13 @@
 package com.example.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Domain.Article;
 import com.example.Repository.ArticleRepository;
 
 @Controller
@@ -12,7 +16,11 @@ public class ArticleController {
 	private ArticleRepository articleRepository;
 	
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList", articleList );
+		
 		return "article";
 	}
 	
