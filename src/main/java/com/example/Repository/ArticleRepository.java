fb727c9092a8a0b4ed9ed8e8cdc2cf9d199b77a6
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-24 14:16:00 +0900

    全記事情報取得method実装完了

---
+++ src/main/java/com/example/Repository/ArticleRepository.java
@@ -0,0 +1,43 @@
+package com.example.Repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.Domain.Article;
+
+/**
+ * 記事を操作するリポジトリ．
+ * 
+ * @author yuiko.mitsui
+ *
+ */
+@Repository
+public class ArticleRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+		Article article = new Article(rs.getInt("id"), rs.getString("name"), rs.getString("content"));
+
+		return article;
+	};
+
+	/**
+	 * 全記事情報を取得する．
+	 * 
+	 * @return 全記事情報
+	 */
+	public List<Article> findAll() {
+
+		String sql = "SELECT id,name,content FROM articles ORDER BY id DESC;";
+
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+
+		return articleList;
+	}
+
+}
